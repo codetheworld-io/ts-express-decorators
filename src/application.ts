1738e78683be6a02fac44a6176bc1acd65df4a3b
hoangdv <hoang.dv@outlook.com>
2021-08-14 12:22:01 +0900

    Update application to reigster routers

--- src/application.ts
+++ src/application.ts
@@ -1,4 +1,7 @@
-import express, { Application as ExApplication } from 'express';
+import express, { Application as ExApplication, Handler } from 'express';
+import { controllers } from './controllers';
+import { MetadataKeys } from './utils/metadata.keys';
+import { IRouter } from './utils/decorators/handlers.decorator';
 
 class Application {
   private readonly _instance: ExApplication;
@@ -9,6 +12,7 @@ class Application {
 
   constructor() {
     this._instance = express();
+    this._instance.use(express.json());
     this.registerRouters();
   }
 
@@ -17,7 +21,29 @@ class Application {
       res.json({ message: 'Hello World!' });
     });
 
-    // TODO: register routers
+    const info: Array<{ api: string, handler: string }> = [];
+
+    controllers.forEach((controllerClass) => {
+      const controllerInstance: { [handleName: string]: Handler } = new controllerClass() as any;
+
+      const basePath: string = Reflect.getMetadata(MetadataKeys.BASE_PATH, controllerClass);
+      const routers: IRouter[] = Reflect.getMetadata(MetadataKeys.ROUTERS, controllerClass);
+
+      const exRouter = express.Router();
+
+      routers.forEach(({ method, path, handlerName}) => {
+        exRouter[method](path, controllerInstance[String(handlerName)].bind(controllerInstance));
+
+        info.push({
+          api: `${method.toLocaleUpperCase()} ${basePath + path}`,
+          handler: `${controllerClass.name}.${String(handlerName)}`,
+        });
+      });
+
+      this._instance.use(basePath, exRouter);
+    });
+
+    console.table(info);
   }
 }
 
